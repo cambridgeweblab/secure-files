7c59e95cd1e8dddf2859cf0796da2e996162136e
Gareth Boden <gareth.boden@tapina.com>
2017-05-30 02:22:50 +0100

    Add TODO notes to check other potential double-URL-encoding problems with toUri() to toUriComponentsBuilder().

--- src/main/java/ucles/weblab/common/files/webapi/DownloadController.java
+++ src/main/java/ucles/weblab/common/files/webapi/DownloadController.java
@@ -33,12 +33,12 @@ public class DownloadController {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private Clock clock = Clock.systemUTC();
     private FileDownloadCache<UUID, PendingDownload> recentDownloadCache;
-    
+
     @Autowired
     public DownloadController(FileDownloadCache recentDownloadCache) {
         this.recentDownloadCache = recentDownloadCache;
     }
-    
+
     /**
      * This method is used by other controllers to generate downloads ready to be fetched by the browser.
      *
@@ -50,41 +50,42 @@ public class DownloadController {
     public URI generateDownload(UUID id, String collectionName, SecureFileEntity secureFile) {
         return recentDownloadCache.getRedirectUrl(id, collectionName, secureFile.getFilename());
     }
-    
+
     /**
      * This method is responsible for making the browser display a file>save as
      * dialog box. If there is no trailing slash, then it does not cause a save-as
-     * box to appear. 
-     * 
+     * box to appear.
+     *
      * @param collectionName
      * @param id
      * @param fileName
-     * @return 
+     * @return
      */
-    @RequestMapping(value = "/{collectionName}/{id}/{fileName}/", 
+    @RequestMapping(value = "/{collectionName}/{id}/{fileName}/",
                     method = RequestMethod.GET)
-    public ResponseEntity<byte[]> fetchPreviouslyGeneratedDownload(@PathVariable String collectionName, 
-                                                                   @PathVariable String id, 
+    public ResponseEntity<byte[]> fetchPreviouslyGeneratedDownload(@PathVariable String collectionName,
+                                                                   @PathVariable String id,
                                                                    @PathVariable String fileName) {
         final UUID downloadId = UUID.fromString(id);
-        
+
         Optional<PendingDownload> pendingDownloadOptional = recentDownloadCache.get(downloadId, collectionName, fileName);
-        
+
         PendingDownload pendingDownload = pendingDownloadOptional.orElse(null);
-                
+
         if (pendingDownload == null || pendingDownload.getPurgeTime().isBefore(Instant.now(clock))) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        
-        URI toUri = linkTo(methodOn(DownloadController.class).fetchPreviouslyGeneratedDownload(downloadId.toString(), collectionName, fileName)).toUri();        
+
+        // TODO: validate that toUri() is OK and doesn't need replacing with URI.create(...toString()) to avoid double-encoding.
+        URI toUri = linkTo(methodOn(DownloadController.class).fetchPreviouslyGeneratedDownload(downloadId.toString(), collectionName, fileName)).toUri();
         HttpHeaders headers = new HttpHeaders();
         // The important thing is to avoid no-cache and no-store, for IE.
-        headers.setCacheControl("private, max-age=300"); 
+        headers.setCacheControl("private, max-age=300");
         headers.setContentType(pendingDownload.getContentType());
         headers.setContentLength(pendingDownload.getContent().length);
         headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + '"');
-        headers.setLocation(toUri);        
+        headers.setLocation(toUri);
         return new ResponseEntity<>(pendingDownload.getContent(), headers, HttpStatus.OK);
-    }        
-    
+    }
+
 }
--- src/main/java/ucles/weblab/common/files/webapi/FileDownloadCacheInMemory.java
+++ src/main/java/ucles/weblab/common/files/webapi/FileDownloadCacheInMemory.java
@@ -26,21 +26,21 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  *
- * @author sukhraj (taken from code from DownloadController with was originally 
+ * @author sukhraj (taken from code from DownloadController with was originally
  * written by gboden.
  */
 public class FileDownloadCacheInMemory implements FileDownloadCache<UUID, PendingDownload> {
-    
+
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    private final ConcurrentHashMap<String, PendingDownload> recentDownloadCache;        
+    private final ConcurrentHashMap<String, PendingDownload> recentDownloadCache;
     private Clock clock = Clock.systemUTC();
     private Duration cacheExpiry;
-    
+
     public FileDownloadCacheInMemory() {
         recentDownloadCache = new ConcurrentHashMap<>();
     }
-    
+
     /**
      * Scheduled job to clean up the cache every 15 minutes.
      */
@@ -55,9 +55,9 @@ public class FileDownloadCacheInMemory implements FileDownloadCache<UUID, Pendin
             }
         }
     }
-       
+
     @Override
-    public Optional<PendingDownload> get(UUID id, String collectionName, String fileName) {        
+    public Optional<PendingDownload> get(UUID id, String collectionName, String fileName) {
         final Iterator<Map.Entry<String, PendingDownload>> cacheEntries = recentDownloadCache.entrySet().iterator();
         while (cacheEntries.hasNext()) {
             Map.Entry<String, PendingDownload> cacheEntry =  cacheEntries.next();
@@ -66,37 +66,37 @@ public class FileDownloadCacheInMemory implements FileDownloadCache<UUID, Pendin
                 return Optional.of(cacheEntry.getValue());
             }
         }
-        
+
         return Optional.empty();
-                    
+
     }
-    
-    
+
+
     @Override
     public Optional<BlobStoreResult> put(UUID id, String collectionName, PendingDownload pendingDownload) {
-        
+
         String key = createCacheKey(id, collectionName, pendingDownload.getFilename());
         PendingDownload result = recentDownloadCache.put(key, pendingDownload );
-        BlobStoreResult blobStoreResult = new BlobStoreResult(new BlobId(id.toString()), 
-                                                              pendingDownload.getFilename(), 
-                                                              collectionName, 
-                                                              pendingDownload.getPurgeTime(), 
+        BlobStoreResult blobStoreResult = new BlobStoreResult(new BlobId(id.toString()),
+                                                              pendingDownload.getFilename(),
+                                                              collectionName,
+                                                              pendingDownload.getPurgeTime(),
                                                               pendingDownload.getUrl()==null ? null : pendingDownload.getUrl().toString());
         return Optional.of(blobStoreResult);
     }
-    
+
     @Override
     public Duration getExpiry() {
         return cacheExpiry;
     }
-    
+
     @Override
     public boolean exists(UUID id, String collectionName, SecureFileMetadata secureFileMetadata) {
         String key = createCacheKey(id, collectionName, collectionName);
         PendingDownload pd = recentDownloadCache.get(key);
         return pd != null;
     }
-    
+
     @Autowired(required = false) // will fall back to default system UTC clock
     public void configureClock(Clock clock) {
         log.warn("Clock overridden with " + clock);
@@ -112,28 +112,30 @@ public class FileDownloadCacheInMemory implements FileDownloadCache<UUID, Pendin
     @Override
     public Optional<URI> getUrl(UUID id, String collectionName, String fileName) {
         try {
+            // TODO: validate that toUri() is OK and doesn't need replacing with URI.create(...toString()) to avoid double-encoding.
             String url = ControllerLinkBuilder.linkTo(methodOn(DownloadController.class).fetchPreviouslyGeneratedDownload(collectionName, id.toString(),fileName )).toUri().toString();
             return Optional.of(URI.create(url));
         } catch (Exception e) {
             log.warn("Exception caught while getting url from ControllerLinkBuilder, returning empty optional", e);
             return Optional.empty();
         }
-        
+
     }
 
     /*@Override
     public Optional<String> getRecentUrl(String collectionName, String fileName) {
-        
+
         //always return empty
         return Optional.empty();
-        
+
     } */
 
     @Override
     public URI getRedirectUrl(UUID id, String collectionName, String fileName) {
+        // TODO: validate that toUri() is OK and doesn't need replacing with URI.create(...toString()) to avoid double-encoding.
         return linkTo(methodOn(DownloadController.class).fetchPreviouslyGeneratedDownload(collectionName, id == null ? null : id.toString(), fileName)).toUri();
     }
 
-    
+
 
 }
