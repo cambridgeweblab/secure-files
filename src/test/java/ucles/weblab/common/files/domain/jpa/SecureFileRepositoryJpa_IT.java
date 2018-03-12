7885816cc02be80a7da80f2de9ee3c43aba1e159
Neale Upstone <neale@nealeupstone.com>
2018-03-12 10:03:50 +0000

    Spring Boot 2 package change fix

--- src/test/java/ucles/weblab/common/files/domain/jpa/SecureFileRepositoryJpa_IT.java
+++ src/test/java/ucles/weblab/common/files/domain/jpa/SecureFileRepositoryJpa_IT.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
