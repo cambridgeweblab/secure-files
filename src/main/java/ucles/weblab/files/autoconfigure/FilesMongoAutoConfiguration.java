91c29b77deb008d50ae95940b66abe71e3c813b0
Neale Upstone <neale@nealeupstone.com>
2021-12-04 11:00:42 +0000

    Import Flapdoodle embedded mongo repository and provide a fake transaction manager

--- src/main/java/ucles/weblab/files/autoconfigure/FilesMongoAutoConfiguration.java
+++ src/main/java/ucles/weblab/files/autoconfigure/FilesMongoAutoConfiguration.java
@@ -1,5 +1,6 @@
 package ucles.weblab.files.autoconfigure;
 
+import de.flapdoodle.embed.mongo.MongodExecutable;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -7,11 +8,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionStatus;
 import ucles.weblab.files.domain.EncryptionService;
 import ucles.weblab.files.domain.FilesFactory;
 import ucles.weblab.files.domain.SecureFileRepository;
@@ -25,22 +35,60 @@ import ucles.weblab.files.domain.mongodb.SecureFileRepositoryMongo;
  * @since 19/06/15
  */
 @Configuration
-@AutoConfigureAfter({MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@AutoConfigureAfter({MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class})
+@Import({MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, FilesAutoConfiguration.class})
 @ConditionalOnClass(FilesFactory.class)
-@ConditionalOnBean(MongoOperations.class)
+//@ConditionalOnBean(MongoTemplate.class)
 @ConditionalOnProperty(prefix = "spring.data.mongodb.repositories", name = "enabled", havingValue = "true", matchIfMissing = true)
 @EnableMongoRepositories(basePackageClasses = {SecureFileCollectionRepositoryMongo.class})
 public class FilesMongoAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(FilesFactory.class)
-    public FilesFactory filesFactoryMongo() {
+    @ConditionalOnMissingBean(FilesFactoryMongo.class)
+    public FilesFactoryMongo filesFactoryMongo() {
         return new FilesFactoryMongo();
     }
 
     @Bean
     @ConditionalOnMissingBean(SecureFileRepository.class)
-    public SecureFileRepository secureFileRepositoryMongo(MongoOperations mongoOperations, GridFsOperations gridFsOperations, EncryptionService encryptionService) {
+    public SecureFileRepository secureFileRepositoryMongo(
+            MongoOperations mongoOperations,
+            GridFsOperations gridFsOperations,
+            EncryptionService encryptionService
+    ) {
         return new SecureFileRepositoryMongo(mongoOperations, gridFsOperations, encryptionService);
+    }
+
+    /**
+     * Provide transaction manager if we're not using Flapdoodle Mongo database
+     * Alternative would be test-containers for real Mongo
+     */
+    @Bean
+    @ConditionalOnMissingBean({MongodExecutable.class})
+    public MongoTransactionManager mongoTransactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+
+    @Bean
+    @ConditionalOnBean(MongodExecutable.class)
+    public PlatformTransactionManager mongoTransactionManager() {
+        return new AbstractPlatformTransactionManager() {
+            @Override
+            protected Object doGetTransaction() throws TransactionException {
+                return new Object();
+            }
+
+            @Override
+            protected void doBegin(Object transaction, TransactionDefinition definition) throws TransactionException {
+            }
+
+            @Override
+            protected void doCommit(DefaultTransactionStatus status) throws TransactionException {
+            }
+
+            @Override
+            protected void doRollback(DefaultTransactionStatus status) throws TransactionException {
+            }
+        };
     }
 }
